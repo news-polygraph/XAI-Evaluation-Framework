8c972b1659c92b8a1a1bc08a53b328a86a09f8bc
naomi-solo <naomisol@stanford.edu>
2024-07-31 11:16:13 +0200

    Cleaning up Visualizations.modules.css

--- src/components/NewsItemComponent.tsx
+++ src/components/NewsItemComponent.tsx
@@ -4,7 +4,6 @@ import { XAIFeatureLevel } from "@/model/xai-feature-level";
 import { useState, useEffect } from "react";
 import ThuthfulnessSlider from "./TruthfulnessSlider";
 import TutorialTooltip from "./TutorialTooltip";
-import images from "./Visualizations.module.css";
 
 const NewsItemComponent = ({
   newsItem,
